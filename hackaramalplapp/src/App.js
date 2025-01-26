ee2ab95c02eb50700b1d711aebce0de3524261c8
Industrial <rmbouzan@gmail.com>
2025-01-25 17:17:17 -0800

    added legacy

--- hackaramalplapp/src/App.js
+++ hackaramalplapp/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { Button } from "@mui/material";
+import {Button, Link} from "@mui/material";
 import PieChart from "./components/PieChart";
+import {Route, Router, Routes} from "react-router-dom";
+import ClientList from "./pages/ClientList";
 
 
 function App() {
@@ -11,6 +13,20 @@ function App() {
         Click Me
       </Button>
         <PieChart />
+    <Router>
+      <div style={{ padding: "20px" }}>
+        <h1>Welcome to My App</h1>
+
+        {/* Hyperlink to the Client List page */}
+        <Link to="/client-list">Go to Client List</Link>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<h2>Home Page</h2>} />
+          <Route path="/client-list" element={<ClientList />} />
+        </Routes>
+      </div>
+    </Router>
     </div>
   );
 }
